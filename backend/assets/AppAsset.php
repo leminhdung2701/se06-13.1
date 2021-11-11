ca7ed40d4d685733a2b40dd940fee0b74a299b11
leminhdung2701 <leminhdung_t64@hus.edu.vn>
2021-11-11 14:36:32 +0700

    khung giao dien admin

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,9 +12,17 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'vendor/fontawesome-free/css/all.min.css',
+        'css/sb-admin-2.min.css',
     ];
     public $js = [
+        'vendor/jquery/jquery.min.js',
+        'vendor/bootstrap/js/bootstrap.bundle.min.js',
+        'vendor/jquery-easing/jquery.easing.min.js',
+        'vendor/chart.js/Chart.min.js',
+        'js/sb-admin-2.min.js',
+        'js/demo/chart-area-demo.js',
+        'js/demo/chart-pie-demo.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
